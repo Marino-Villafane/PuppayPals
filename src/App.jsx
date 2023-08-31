7d38ce850a65e897267f6dc7ed4c9d184e56d719
root <root@DESKTOP-L7V09QC.localdomain>
2023-08-31 17:38:15 -0500

    log puppy id onClick

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,27 @@ import { useState } from 'react'
 import {puppyList} from './data.js'
 import './App.css'
 
-function App() {
 
-// Get a list of the puppies to the website
+
+function App() {
+ // Get a list of the puppies to the website
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
 
-  function handleClick(puppyId) {
-    setFeatPupId(puppyId); // Update the featPupId state with the clicked puppy's ID
+  function handleClick() {
+    // some logic here
   }
- 
-  
+
   return (
-      <div className='App'>
-       
-        {puppies.map((puppy) => {
-          return <p onClick={handleClick} key={puppy.id}>{puppy.name}</p>
-        })
-        }
-        {featPupId && <p>{featPupId}</p>}
-      </div>
-  
+    <div className="App">
+      {puppies.map((puppy) => (
+        <p onClick={() => {console.log("puppy id: ", puppy.id)}} key={puppy.id}>
+          {puppy.name}
+        </p>
+      ))}
+    </div>
   );
 }
+
 
 export default App

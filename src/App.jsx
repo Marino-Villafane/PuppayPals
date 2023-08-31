ecac692f7550d9aadd70923d93776f0621409cd4
root <root@DESKTOP-L7V09QC.localdomain>
2023-08-31 16:44:37 -0500

    Made changes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {puppyList} from './data.js'
 import './App.css'
 
 function App() {
 
-
+// Get a list of the puppies to the website
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log("puppyList: ", puppyList);
   return (
-    <>
-      <div>
-       
+      <div className='App'>
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+        }
       </div>
   
-    </>
-  )
+  );
 }
 
 export default App

d0fd39718307ca4f5c2d321244f07a21b4e89074
root <root@DESKTOP-L7V09QC.localdomain>
2023-08-31 17:15:34 -0500

    added function handleClick

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,20 @@ function App() {
 // Get a list of the puppies to the website
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
-  console.log("puppyList: ", puppyList);
+
+  function handleClick(puppyId) {
+    setFeatPupId(puppyId); // Update the featPupId state with the clicked puppy's ID
+  }
+ 
+  
   return (
       <div className='App'>
+       
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick={handleClick} key={puppy.id}>{puppy.name}</p>
         })
         }
+        {featPupId && <p>{featPupId}</p>}
       </div>
   
   );

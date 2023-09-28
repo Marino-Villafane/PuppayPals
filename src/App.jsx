907bcc0459d203a260445cf867432210477dc617
root <root@DESKTOP-L7V09QC>
2023-09-28 12:31:36 -0500

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
  // Get a list of the puppies to the website
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
 
   function handleClick() {
     // some logic here
@@ -16,10 +17,23 @@ function App() {
   return (
     <div className="App">
       {puppies.map((puppy) => (
-        <p onClick={() => {console.log("puppy id: ", puppy.id)}} key={puppy.id}>
+        <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>
           {puppy.name}
         </p>
+
+       
       ))}
+      
+      <p>
+        {featPupId && <p> {featPupId } 
+        <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+            </div></p>  } 
+      </p>
     </div>
   );
 }
